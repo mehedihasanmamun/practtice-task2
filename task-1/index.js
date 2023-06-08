5a5668dbdf2349d436c63a235aa43457c3490140
mehedihasanmamun <75701729+mehedihasanmamun@users.noreply.github.com>
2023-06-08 23:14:56 +0600

    Add files via upload

---
+++ task-1/index.js
@@ -0,0 +1,43 @@
+
+const users = [
+    {
+      name: "Jason",
+      type: "member",
+      price: 39,
+      priceType: "monthly",
+      category: ["clothing", "food", "medicine"],
+      brand: "aroma",
+      status: "published",
+      color: ["red", "green", "blue"],
+      products: [
+        {name: "product one", type: "clothing", price: 19, category: "men" },
+        {name: "product two", type: "clothing", price: 29, category: "women" },
+        {name: "product three", type: "food", price: 9, category: "liquid" },
+        {name: "product four", type: "medicine", price: 3.5, category: "heart" },
+      ]
+    },
+    {
+      name: "John Doe",
+      type: "customer",
+      price: 0,
+      priceType: "",
+      category: ["food", "medicine"],
+      brand: "typhon",
+      status: "new",
+      color: ["yellow", "black", "orange"],
+      products: [
+        {name: "product one", type: "clothing", price: 19, category: "men" },
+        {name: "product two", type: "clothing", price: 29, category: "women" },
+        {name: "product three", type: "food", price: 9, category: "liquid" },
+      ]
+    }
+  ];
+
+
+  const usersWithBlueColor = users.filter(user => user.color.includes("blue"));
+
+   
+
+  const userNamesWithBlueColor = usersWithBlueColor.map(user => user.name);
+  
+  console.log(userNamesWithBlueColor);
